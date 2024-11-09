fd34b73eb522d7ab995b6b3bdf8c83bfb2ae090a
Dlmobile98 <164272159+Dlmobile98@users.noreply.github.com>
2024-11-09 15:33:15 +0700

    Update Peekpoke.Designer.cs

--- Forms/Peekpoke.Designer.cs
+++ Forms/Peekpoke.Designer.cs
@@ -6,27 +6,27 @@ namespace FirehoseFinder
         /// <summary>
         /// Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components = null;
+      //  private System.ComponentModel.IContainer components = null;
 
         /// <summary>
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        /// protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
+            //if (disposing && (components != null))
             {
                 components.Dispose();
             }
-            base.Dispose(disposing);
+            //base.Dispose(disposing);
         }
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+         <summary>
+         Required method for Designer support - do not modify
+         the contents of this method with the code editor.
+         </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
